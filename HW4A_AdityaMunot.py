ea51fe4f9c6c453b3ab39330762cf39c8ad688b6
Aditya Munot <munot4@gmail.com>
2019-02-19 21:01:26 -0500

    update

--- HW4A_AdityaMunot.py
+++ HW4A_AdityaMunot.py
@@ -22,16 +22,16 @@ def Repocheck(UserID):
     return repos
 
 
-def Repoprint(repos):
-    for i in repos:
-        print(f"Repo: {i[0]} Number of commits: {i[1]}")
-    print(repos)
+#def Repoprint(repos):
+#    for i in repos:
+#        print(f"Repo: {i[0]} Number of commits: {i[1]}")
+#    print(repos)
 
 
 def main():
     UserID = input("Enter GitHub user ID: ")
     repos = Repocheck(UserID)
-    Repoprint(repos)
+    #Repoprint(repos)
 
 # Uncomment this below line to use this py file
 #if __name__ == "__main__":
